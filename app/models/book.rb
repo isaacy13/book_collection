11b7670aa5fb338bd6951e7d362744028eaf9b84
isaacy13 <iyastros@gmail.com>
2022-02-04 14:16:37 -0600

    implemented multi-books exercise

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,5 @@ class Book < ApplicationRecord
     validates :author, presence: true, format: { with: /[^( )][0-9a-zA-Z( )]+[^( )]/, message: "letters and in-between spaces only" }
     validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :publish_date, presence: true
-    belongs_to :user
+    has_many :users, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_193241) do
+ActiveRecord::Schema.define(version: 2022_02_04_194150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2022_02_04_193241) do
     t.string "author"
     t.float "price"
     t.date "publish_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_books", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
